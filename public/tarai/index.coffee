8f3e7cc9e064204db52181242b2a997f35c67a1a
mohayonao <mohayonao@gmail.com>
2013-04-17 23:22:51 +0900

    add /tarai/

---
+++ public/tarai/index.coffee
@@ -0,0 +1,65 @@
+$ ->
+  'use strict'
+
+  sc.use 'prototype'
+
+  Array::sorted = ->
+    do @.sort
+    @
+  
+  class Tarai
+    init: (x, y, z)->
+      @list = []
+      @index = 0
+      @tarai x, y, z
+
+    tarai: (x, y, z)->
+      @list.push [ x, y, z ]
+      if x <= y then y
+      else @tarai @tarai(x-1, y, z), @tarai(y-1, z, x), @tarai(z-1, x, y)
+
+    fetch: ->
+      @list[@index++]
+
+    reset: ->
+      @index = 0
+
+  tarai = new Tarai
+  tarai.init 10, 5, 0
+  
+  scale = sc.Scale.kumoi()
+  baseRoot  = 62 # D3
+  
+  pattern = [ 0,0,1,1,2,2,1,1,0,0,1,1,2,2,1,1 ]
+  car = []
+
+  synth  = T('PluckGen')
+  master = T('chorus', delay:4, rate:1, depth:40, synth)
+  master = T('delay', time:'bpm120 l8.', fb:0.4, mix:0.3, master)
+
+  timer = T('interval', interval:'bpm120 l16', (count)->
+    count &= 15
+
+    if count is 0
+      car = tarai.fetch()?.sorted()
+
+    if car
+      i = car[pattern[count]]
+      noteNum = Math.round( scale.performDegreeToKey(i) ) + baseRoot
+      noteNum += 12 * (count % 2)
+
+      synth.noteOn noteNum, 100
+  )
+
+  isPlaying = false
+  $('#play').on 'click', ->
+    isPlaying = not isPlaying
+    if isPlaying
+      tarai.reset()
+      master.play()
+      timer.start()
+      $(this).css 'color', 'red'
+    else
+      master.pause()
+      timer.stop()
+      $(this).css 'color', 'black'
---
+++ public/tarai/index.js
@@ -0,0 +1,89 @@
+(function() {
+  $(function() {
+    'use strict';
+    var Tarai, baseRoot, car, isPlaying, master, pattern, scale, synth, tarai, timer;
+
+    sc.use('prototype');
+    Array.prototype.sorted = function() {
+      this.sort();
+      return this;
+    };
+    Tarai = (function() {
+      function Tarai() {}
+
+      Tarai.prototype.init = function(x, y, z) {
+        this.list = [];
+        this.index = 0;
+        return this.tarai(x, y, z);
+      };
+
+      Tarai.prototype.tarai = function(x, y, z) {
+        this.list.push([x, y, z]);
+        if (x <= y) {
+          return y;
+        } else {
+          return this.tarai(this.tarai(x - 1, y, z), this.tarai(y - 1, z, x), this.tarai(z - 1, x, y));
+        }
+      };
+
+      Tarai.prototype.fetch = function() {
+        return this.list[this.index++];
+      };
+
+      Tarai.prototype.reset = function() {
+        return this.index = 0;
+      };
+
+      return Tarai;
+
+    })();
+    tarai = new Tarai;
+    tarai.init(10, 5, 0);
+    scale = sc.Scale.kumoi();
+    baseRoot = 62;
+    pattern = [0, 0, 1, 1, 2, 2, 1, 1, 0, 0, 1, 1, 2, 2, 1, 1];
+    car = [];
+    synth = T('PluckGen');
+    master = T('chorus', {
+      delay: 4,
+      rate: 1,
+      depth: 40
+    }, synth);
+    master = T('delay', {
+      time: 'bpm120 l8.',
+      fb: 0.4,
+      mix: 0.3
+    }, master);
+    timer = T('interval', {
+      interval: 'bpm120 l16'
+    }, function(count) {
+      var i, noteNum, _ref;
+
+      count &= 15;
+      if (count === 0) {
+        car = (_ref = tarai.fetch()) != null ? _ref.sorted() : void 0;
+      }
+      if (car) {
+        i = car[pattern[count]];
+        noteNum = Math.round(scale.performDegreeToKey(i)) + baseRoot;
+        noteNum += 12 * (count % 2);
+        return synth.noteOn(noteNum, 100);
+      }
+    });
+    isPlaying = false;
+    return $('#play').on('click', function() {
+      isPlaying = !isPlaying;
+      if (isPlaying) {
+        tarai.reset();
+        master.play();
+        timer.start();
+        return $(this).css('color', 'red');
+      } else {
+        master.pause();
+        timer.stop();
+        return $(this).css('color', 'black');
+      }
+    });
+  });
+
+}).call(this);
