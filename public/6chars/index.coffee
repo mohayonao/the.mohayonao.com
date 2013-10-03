5247a59b86718fcb560a2f6437701df1b3177150
mohayonao <mohayonao@gmail.com>
2013-10-04 07:14:51 +0900

    up

--- public/6chars/index.coffee
+++ public/6chars/index.coffee
@@ -4,12 +4,30 @@ $ ->
   WavDecoder.load("./drumkit.wav").then (wav)->
     waves = []
     len = wav.buffer[0].length >> 2
-    waves[0] = wav.buffer[0].subarray len * 0, len * 1 - 1024
-    waves[1] = wav.buffer[0].subarray len * 1, len * 2 - 1024
-    waves[2] = wav.buffer[0].subarray len * 2, len * 3 - 1024
+    waves[0] = wav.buffer[0].subarray len * 0, len * 1
+    waves[1] = wav.buffer[0].subarray len * 1, len * 2
+    waves[2] = wav.buffer[0].subarray len * 2, len * 3
     waves.samplerate = wav.samplerate
 
     hrm = new HexRhythmMachine(pico.samplerate, waves)
 
-    hrm.setPattern "aa0980 aa08a2"
-    pico.play hrm
+    isPlaying = false
+    $('#play').on 'click', ->
+      isPlaying = not isPlaying
+      if isPlaying
+        hrm.setPattern $("#p").val()
+        pico.play(hrm)
+        $(this).css 'color', 'red'
+      else
+        pico.pause()
+        $(this).css 'color', 'black'
+
+    prev = $("#p").val()
+    $("#p").on 'keyup', ->
+      val = $("#p").val()
+      if val isnt prev
+        hrm.setPattern val
+      prev = val
+
+  0
+
--- public/6chars/index.js
+++ public/6chars/index.js
@@ -1,18 +1,39 @@
 (function() {
   $(function() {
-    'use strict';    return WavDecoder.load("./drumkit.wav").then(function(wav) {
-      var hrm, len, waves;
+    'use strict';    WavDecoder.load("./drumkit.wav").then(function(wav) {
+      var hrm, isPlaying, len, prev, waves;
 
       waves = [];
       len = wav.buffer[0].length >> 2;
-      waves[0] = wav.buffer[0].subarray(len * 0, len * 1 - 1024);
-      waves[1] = wav.buffer[0].subarray(len * 1, len * 2 - 1024);
-      waves[2] = wav.buffer[0].subarray(len * 2, len * 3 - 1024);
+      waves[0] = wav.buffer[0].subarray(len * 0, len * 1);
+      waves[1] = wav.buffer[0].subarray(len * 1, len * 2);
+      waves[2] = wav.buffer[0].subarray(len * 2, len * 3);
       waves.samplerate = wav.samplerate;
       hrm = new HexRhythmMachine(pico.samplerate, waves);
-      hrm.setPattern("aa0980 aa08a2");
-      return pico.play(hrm);
+      isPlaying = false;
+      $('#play').on('click', function() {
+        isPlaying = !isPlaying;
+        if (isPlaying) {
+          hrm.setPattern($("#p").val());
+          pico.play(hrm);
+          return $(this).css('color', 'red');
+        } else {
+          pico.pause();
+          return $(this).css('color', 'black');
+        }
+      });
+      prev = $("#p").val();
+      return $("#p").on('keyup', function() {
+        var val;
+
+        val = $("#p").val();
+        if (val !== prev) {
+          hrm.setPattern(val);
+        }
+        return prev = val;
+      });
     });
+    return 0;
   });
 
 }).call(this);
