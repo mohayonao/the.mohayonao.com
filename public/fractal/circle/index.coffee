da89ded4358df5c677d1ec236c67f4e05c5781bc
mohayonao <mohayonao@gmail.com>
2014-07-31 11:07:56 +0900

    update use vue.js

--- public/fractal/circle/index.coffee
+++ public/fractal/circle/index.coffee
@@ -7,40 +7,8 @@ $ ->
 
   context = canvas.getContext '2d'
 
-  circle = (x, y, r)->
-    context.beginPath()
-    context.arc x, y, r, 0, Math.PI * 2, true
-    context.closePath()
-    context.stroke()
-
-  reqId = 0
-  queue = []
-
-  animate = ->
-    for i in [0..256]
-      f = queue.pop()
-      if _.isFunction f
-        f()
-      else if _.isArray f
-        [ x, y, r, sr, br, n ] = f
-        in_circle x, y, r, sr, br, n
-
-    if queue.length isnt 0
-      reqId = requestAnimationFrame animate
-
-  in_circle = (x, y, r, sr, br, n)->
-    if --n < 0 or r < 2 then return
-
-    circle 320 + x, 320 - y, r
-
-    do (x, y, r, sr, br, n)->
-      queue.push ->
-        th = Math.PI * 2.0 / br
-        for k in [0...br] by 1
-          nr = r / sr
-          nx = nr * (sr - 1.0) * Math.cos(th * k) + x
-          ny = nr * (sr - 1.0) * Math.sin(th * k) + y
-          queue.push [ nx, ny, nr, sr, br, n - 1 ]
+  clip = (num, min, max)->
+    Math.max(min, Math.min(num, max))
 
   linlin = (num, inMin, inMax, outMin, outMax)->
     (num - inMin) / (inMax - inMin) * (outMax - outMin) + outMin
@@ -48,55 +16,101 @@ $ ->
   linexp = (num, inMin, inMax, outMin, outMax)->
     Math.pow(outMax / outMin, (num - inMin) / (inMax - inMin)) * outMin
 
-  hsv2rgb = (h, s, v)->
-    r = g = b = v
+  class Application
+    constructor: (@context, @width, @height)->
+      @reqId = 0
+      @stack = []
 
-    if s isnt 0.0
-      h = h * 6
-      f = h - (h|0)
+    init: (width, color)->
+      @clear()
+      @context.lineWidth   = width
+      @context.strokeStyle = color
 
-      switch h|0
-        when 0
-          g *= 1 - s * (1 - f)
-          b *= 1 - s
-        when 1
-          r *= 1 - s * f
-          b *= 1 - s
-        when 2
-          r *= 1 - s
-          b *= 1 - s * (1 - f)
-        when 3
-          r *= 1 - s
-          g *= 1 - s * f
-        when 4
-          r *= 1 - s * (1 - f)
-          g *= 1 - s
-        when 5
-          g *= 1 - s
-          b *= 1 - s * f
+    clear: ->
+      cancelAnimationFrame @reqId
+      context.clearRect 0, 0, @.width, @height
+      @reqId = 0
+      @stack = []
 
-    [ r, g, b ].map (x)-> Math.round(x * 255)
+    generate: (x, y, r, sr, br, n)->
+      @stack = [ [ next, x, y, r, sr, br, n ] ]
+      @reqId = requestAnimationFrame => animate.call(@)
 
-  update = ->
-    cancelAnimationFrame reqId
+    circle = (context, x, y, r)->
+      context.beginPath()
+      context.arc x, y, r, 0, Math.PI * 2, true
+      context.closePath()
+      context.stroke()
 
-    context.clearRect 0, 0, canvas.width, canvas.height
+    animate = ->
+      for i in [0..256]
+        break if @stack.length is 0
 
-    sr = Math.max(1, Math.min($('#sr').val(), 100))
-    br = Math.max(1, Math.min($('#br').val(), 100))
-    n  = Math.max(2, Math.min($('#n' ).val(),  12))
-    h  = Math.max(1, Math.min($('#h' ).val(), 100))
+        items = @stack.pop()
+        func  = _.first(items)
+        func.apply @, _.rest(items)
 
-    sr = linexp sr, 100, 1, 1.0, 10.0
-    h  = linlin h , 0, 100, 0, 1.0
-    context.strokeStyle = "rgb(" + hsv2rgb(h, 0.5, 0.8).join(',') + ")"
+      if @stack.length isnt 0
+        @reqId = requestAnimationFrame => animate.call(@)
 
-    queue = [ [ 0, 0, 300, sr, br, n ] ]
-    reqId = requestAnimationFrame animate
+    next = (x, y, r, sr, br, n)->
+      circle @context, @width * 0.5 + x, @height * 0.5 - y, r
 
-  update()
+      nr = r / sr
+      if n > 1 and nr > 2
+        func = (x, y, nr, sr, br, n)=>
+          th = Math.PI * 2.0 / br
+          for k in [0...br] by 1
+            nx = nr * (sr - 1.0) * Math.cos(th * k) + x
+            ny = nr * (sr - 1.0) * Math.sin(th * k) + y
+            @stack.push [ next, nx, ny, nr, sr, br, n - 1 ]
+        @stack.push [ func, x, y, nr, sr, br, n ]
 
-  $('#sr').on 'input', update
-  $('#br').on 'input', update
-  $('#n' ).on 'input', update
-  $('#h' ).on 'input', update
+  app = new Application(context, canvas.width, canvas.height)
+
+  vue = new Vue
+    el: '#app'
+
+    data:
+      width : app.width
+      height: app.height
+      params: [
+        { label: 'sr', value: 64 }
+        { label: 'br', value: 36 }
+        { label: 'n' , value:  5 }
+        { label: 'h' , value:  1 }
+      ]
+
+    methods:
+      update: ->
+        [ sr, br, n, h ] = vue.getParams()
+
+        hsv = color.hsv2rgb h, 0.5, 0.8
+
+        app.init 0.2, "rgb(" + hsv.join(',') + ")"
+        app.generate 0, 0, 300, sr, br, n
+
+      getParams: ->
+        sr = clip @params[0].value, 1, 100
+        br = clip @params[1].value, 1, 100
+        n  = clip @params[2].value, 1, 100
+        h  = clip @params[3].value, 1, 100
+
+        window.location.replace "#" + [ sr, br, n, h ].join(',')
+
+        sr = linexp(sr, 100, 1, 1.0, 10.0)
+        br = br|0
+        n  = linlin(n , 1, 100, 2, 24)|0
+        h  = linlin(h , 1, 100, 0, 360)
+
+        [ sr, br, n, h ]
+
+  if window.location.hash
+    hash = decodeURIComponent(window.location.hash.substr(1))
+    items = hash.split ','
+    vue.params[0].value = (items[0]|0)
+    vue.params[1].value = (items[1]|0)
+    vue.params[2].value = (items[2]|0)
+    vue.params[3].value = (items[3]|0)
+
+  vue.update()
--- public/fractal/circle/index.js
+++ public/fractal/circle/index.js
@@ -1,53 +1,13 @@
 (function() {
   $(function() {
     'use strict';
-    var animate, canvas, circle, context, hsv2rgb, in_circle, linexp, linlin, queue, reqId, update;
+    var Application, app, canvas, clip, context, hash, items, linexp, linlin, vue;
     canvas = document.getElementById('canvas');
     canvas.width = 640;
     canvas.height = 640;
     context = canvas.getContext('2d');
-    circle = function(x, y, r) {
-      context.beginPath();
-      context.arc(x, y, r, 0, Math.PI * 2, true);
-      context.closePath();
-      return context.stroke();
-    };
-    reqId = 0;
-    queue = [];
-    animate = function() {
-      var br, f, i, n, r, sr, x, y, _i;
-      for (i = _i = 0; _i <= 256; i = ++_i) {
-        f = queue.pop();
-        if (_.isFunction(f)) {
-          f();
-        } else if (_.isArray(f)) {
-          x = f[0], y = f[1], r = f[2], sr = f[3], br = f[4], n = f[5];
-          in_circle(x, y, r, sr, br, n);
-        }
-      }
-      if (queue.length !== 0) {
-        return reqId = requestAnimationFrame(animate);
-      }
-    };
-    in_circle = function(x, y, r, sr, br, n) {
-      if (--n < 0 || r < 2) {
-        return;
-      }
-      circle(320 + x, 320 - y, r);
-      return (function(x, y, r, sr, br, n) {
-        return queue.push(function() {
-          var k, nr, nx, ny, th, _i, _results;
-          th = Math.PI * 2.0 / br;
-          _results = [];
-          for (k = _i = 0; _i < br; k = _i += 1) {
-            nr = r / sr;
-            nx = nr * (sr - 1.0) * Math.cos(th * k) + x;
-            ny = nr * (sr - 1.0) * Math.sin(th * k) + y;
-            _results.push(queue.push([nx, ny, nr, sr, br, n - 1]));
-          }
-          return _results;
-        });
-      })(x, y, r, sr, br, n);
+    clip = function(num, min, max) {
+      return Math.max(min, Math.min(num, max));
     };
     linlin = function(num, inMin, inMax, outMin, outMax) {
       return (num - inMin) / (inMax - inMin) * (outMax - outMin) + outMin;
@@ -55,61 +15,144 @@
     linexp = function(num, inMin, inMax, outMin, outMax) {
       return Math.pow(outMax / outMin, (num - inMin) / (inMax - inMin)) * outMin;
     };
-    hsv2rgb = function(h, s, v) {
-      var b, f, g, r;
-      r = g = b = v;
-      if (s !== 0.0) {
-        h = h * 6;
-        f = h - (h | 0);
-        switch (h | 0) {
-          case 0:
-            g *= 1 - s * (1 - f);
-            b *= 1 - s;
+    Application = (function() {
+      var animate, circle, next;
+
+      function Application(context, width, height) {
+        this.context = context;
+        this.width = width;
+        this.height = height;
+        this.reqId = 0;
+        this.stack = [];
+      }
+
+      Application.prototype.init = function(width, color) {
+        this.clear();
+        this.context.lineWidth = width;
+        return this.context.strokeStyle = color;
+      };
+
+      Application.prototype.clear = function() {
+        cancelAnimationFrame(this.reqId);
+        context.clearRect(0, 0, this.width, this.height);
+        this.reqId = 0;
+        return this.stack = [];
+      };
+
+      Application.prototype.generate = function(x, y, r, sr, br, n) {
+        this.stack = [[next, x, y, r, sr, br, n]];
+        return this.reqId = requestAnimationFrame((function(_this) {
+          return function() {
+            return animate.call(_this);
+          };
+        })(this));
+      };
+
+      circle = function(context, x, y, r) {
+        context.beginPath();
+        context.arc(x, y, r, 0, Math.PI * 2, true);
+        context.closePath();
+        return context.stroke();
+      };
+
+      animate = function() {
+        var func, i, items, _i;
+        for (i = _i = 0; _i <= 256; i = ++_i) {
+          if (this.stack.length === 0) {
             break;
-          case 1:
-            r *= 1 - s * f;
-            b *= 1 - s;
-            break;
-          case 2:
-            r *= 1 - s;
-            b *= 1 - s * (1 - f);
-            break;
-          case 3:
-            r *= 1 - s;
-            g *= 1 - s * f;
-            break;
-          case 4:
-            r *= 1 - s * (1 - f);
-            g *= 1 - s;
-            break;
-          case 5:
-            g *= 1 - s;
-            b *= 1 - s * f;
+          }
+          items = this.stack.pop();
+          func = _.first(items);
+          func.apply(this, _.rest(items));
+        }
+        if (this.stack.length !== 0) {
+          return this.reqId = requestAnimationFrame((function(_this) {
+            return function() {
+              return animate.call(_this);
+            };
+          })(this));
+        }
+      };
+
+      next = function(x, y, r, sr, br, n) {
+        var func, nr;
+        circle(this.context, this.width * 0.5 + x, this.height * 0.5 - y, r);
+        nr = r / sr;
+        if (n > 1 && nr > 2) {
+          func = (function(_this) {
+            return function(x, y, nr, sr, br, n) {
+              var k, nx, ny, th, _i, _results;
+              th = Math.PI * 2.0 / br;
+              _results = [];
+              for (k = _i = 0; _i < br; k = _i += 1) {
+                nx = nr * (sr - 1.0) * Math.cos(th * k) + x;
+                ny = nr * (sr - 1.0) * Math.sin(th * k) + y;
+                _results.push(_this.stack.push([next, nx, ny, nr, sr, br, n - 1]));
+              }
+              return _results;
+            };
+          })(this);
+          return this.stack.push([func, x, y, nr, sr, br, n]);
+        }
+      };
+
+      return Application;
+
+    })();
+    app = new Application(context, canvas.width, canvas.height);
+    vue = new Vue({
+      el: '#app',
+      data: {
+        width: app.width,
+        height: app.height,
+        params: [
+          {
+            label: 'sr',
+            value: 64
+          }, {
+            label: 'br',
+            value: 36
+          }, {
+            label: 'n',
+            value: 5
+          }, {
+            label: 'h',
+            value: 1
+          }
+        ]
+      },
+      methods: {
+        update: function() {
+          var br, h, hsv, n, sr, _ref;
+          _ref = vue.getParams(), sr = _ref[0], br = _ref[1], n = _ref[2], h = _ref[3];
+          hsv = color.hsv2rgb(h, 0.5, 0.8);
+          app.init(0.2, "rgb(" + hsv.join(',') + ")");
+          return app.generate(0, 0, 300, sr, br, n);
+        },
+        getParams: function() {
+          var br, h, n, sr;
+          sr = clip(this.params[0].value, 1, 100);
+          br = clip(this.params[1].value, 1, 100);
+          n = clip(this.params[2].value, 1, 100);
+          h = clip(this.params[3].value, 1, 100);
+          window.location.replace("#" + [sr, br, n, h].join(','));
+          sr = linexp(sr, 100, 1, 1.0, 10.0);
+          br = br | 0;
+          n = linlin(n, 1, 100, 2, 24) | 0;
+          h = linlin(h, 1, 100, 0, 360);
+          return [sr, br, n, h];
         }
       }
-      return [r, g, b].map(function(x) {
-        return Math.round(x * 255);
-      });
-    };
-    update = function() {
-      var br, h, n, sr;
-      cancelAnimationFrame(reqId);
-      context.clearRect(0, 0, canvas.width, canvas.height);
-      sr = Math.max(1, Math.min($('#sr').val(), 100));
-      br = Math.max(1, Math.min($('#br').val(), 100));
-      n = Math.max(2, Math.min($('#n').val(), 12));
-      h = Math.max(1, Math.min($('#h').val(), 100));
-      sr = linexp(sr, 100, 1, 1.0, 10.0);
-      h = linlin(h, 0, 100, 0, 1.0);
-      context.strokeStyle = "rgb(" + hsv2rgb(h, 0.5, 0.8).join(',') + ")";
-      queue = [[0, 0, 300, sr, br, n]];
-      return reqId = requestAnimationFrame(animate);
-    };
-    update();
-    $('#sr').on('input', update);
-    $('#br').on('input', update);
-    $('#n').on('input', update);
-    return $('#h').on('input', update);
+    });
+    if (window.location.hash) {
+      hash = decodeURIComponent(window.location.hash.substr(1));
+      items = hash.split(',');
+      vue.params[0].value = items[0] | 0;
+      vue.params[1].value = items[1] | 0;
+      vue.params[2].value = items[2] | 0;
+      vue.params[3].value = items[3] | 0;
+    }
+    return vue.update();
   });
 
 }).call(this);
