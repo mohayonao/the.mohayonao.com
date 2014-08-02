ac894f3b45f92b8c8e1e0b80051640d8a16837bc
mohayonao <mohayonao@gmail.com>
2014-08-02 13:35:00 +0900

    update tarai-music

--- public/tarai/index.coffee
+++ public/tarai/index.coffee
@@ -1,65 +1,112 @@
-$ ->
-  'use strict'
+'use strict'
 
-  sc.use 'prototype'
+clip = (num, min, max)->
+  Math.max(min, Math.min(num, max))
 
-  Array::sorted = ->
-    do @.sort
-    @
-  
-  class Tarai
-    init: (x, y, z)->
-      @list = []
-      @index = 0
-      @tarai x, y, z
+class Tarai
+  init: (x, y, z)->
+    @list = []
+    @index = 0
+    @tarai x, y, z
 
-    tarai: (x, y, z)->
-      @list.push [ x, y, z ]
-      if x <= y then y
-      else @tarai @tarai(x-1, y, z), @tarai(y-1, z, x), @tarai(z-1, x, y)
+  tarai: (x, y, z)->
+    @list.push [ x, y, z ]
+    if x <= y then y
+    else @tarai @tarai(x-1, y, z), @tarai(y-1, z, x), @tarai(z-1, x, y)
 
-    fetch: ->
-      @list[@index++]
+  fetch: ->
+    @list[@index++]
 
-    reset: ->
-      @index = 0
+  reset: ->
+    @index = 0
 
-  tarai = new Tarai
-  tarai.init 10, 5, 0
-  
-  scale = new sc.Scale [0,2,3,7,9], 12, 'Kumoi'
-  baseRoot  = 62 # D3
-  
-  pattern = [ 0,0,1,1,2,2,1,1,0,0,1,1,2,2,1,1 ]
-  car = []
+tarai = new Tarai
+scale = new sc.Scale [ 0, 2, 3, 7, 9 ], 12, 'Kumoi'
+baseRoot  = 62 # D3
 
-  synth  = T('PluckGen')
-  master = T('chorus', delay:4, rate:1, depth:40, synth)
-  master = T('delay', time:'bpm120 l8.', fb:0.4, mix:0.3, master)
+pattern = [ 0, 0, 1, 1, 2, 2, 1, 1, 0, 0, 1, 1, 2, 2, 1, 1 ]
+car     = []
 
-  timer = T('interval', interval:'bpm120 l16', (count)->
-    count &= 15
+synth  = T('PluckGen')
+master = T('chorus', delay:4, rate:1, depth:40, synth)
+master = T('delay', time:'bpm120 l8.', fb:0.4, mix:0.3, master)
 
-    if count is 0
-      car = tarai.fetch()?.sorted()
+timer = T('interval', interval:'bpm120 l16', (count)->
+  i = count & 15
 
-    if car
-      i = car[pattern[count]]
-      noteNum = Math.round( scale.performDegreeToKey(i) ) + baseRoot
-      noteNum += 12 * (count % 2)
+  if i is 0
+    car = tarai.fetch()?.sort()
 
-      synth.noteOn noteNum, 100
-  )
+  if car
+    p = pattern[i]
+    i = car[p]
+    switch p
+      when 0 then vue.x = clip(i, 0, 10)
+      when 1 then vue.y = clip(i, 0, 10)
+      when 2 then vue.z = clip(i, 0, 10)
+    noteNum = Math.round( scale.performDegreeToKey(i) ) + baseRoot
+    noteNum += 12 * (count % 2)
 
-  isPlaying = false
-  $('#play').on 'click', ->
-    isPlaying = not isPlaying
-    if isPlaying
-      tarai.reset()
-      master.play()
-      timer.start()
-      $(this).addClass 'btn-active'
-    else
-      master.pause()
-      timer.stop()
-      $(this).removeClass 'btn-active'
+    synth.noteOn noteNum, 100
+)
+
+app = new class
+  constructor: ->
+    @isPlaying = false
+
+  init: (x, y, z)->
+    tarai.init x, y, z
+
+  play: ->
+    @isPlaying = true
+    tarai.reset()
+    master.play()
+    timer.start()
+
+  stop: ->
+    @isPlaying = false
+    master.pause()
+    timer.stop()
+
+vue = new Vue
+  el: '#app'
+
+  data:
+    x : 10
+    y :  5
+    z :  0
+    xs: _.range(11).map (x)-> value: x
+    ys: _.range(11).map (y)-> value: y
+    zs: _.range(11).map (z)-> value: z
+    isPlaying: app.isPlaying
+
+  methods:
+    play: ->
+      if app.isPlaying
+        app.stop()
+      else
+        window.location.replace "##{@x},#{@y},#{@z}"
+        app.init @x, @y, @z
+        app.play()
+      @isPlaying = app.isPlaying
+
+    stop: ->
+      if app.isPlaying
+        app.stop()
+      @isPlaying = app.isPlaying
+
+    tweet: ->
+      x = clip(@x, 0, 10)
+      y = clip(@y, 0, 10)
+      z = clip(@z, 0, 10)
+      text  = 'tarai-music'
+      url   = "http://#{location.host}/tarai/##{x},#{y},#{z}"
+      utils.tweet text:text, url:url
+
+window.onhashchange = ->
+  items = decodeURI(location.hash.substr(1).trim()).split(',')
+  vue.x = clip(items[0]|0, 0, 10)
+  vue.y = clip(items[1]|0, 0, 10)
+  vue.z = clip(items[2]|0, 0, 10)
+
+window.onhashchange() if location.hash
--- public/tarai/index.js
+++ public/tarai/index.js
@@ -1,87 +1,183 @@
 (function() {
-  $(function() {
-    'use strict';
-    var Tarai, baseRoot, car, isPlaying, master, pattern, scale, synth, tarai, timer;
-    sc.use('prototype');
-    Array.prototype.sorted = function() {
-      this.sort();
-      return this;
+  'use strict';
+  var Tarai, app, baseRoot, car, clip, master, pattern, scale, synth, tarai, timer, vue;
+
+  clip = function(num, min, max) {
+    return Math.max(min, Math.min(num, max));
+  };
+
+  Tarai = (function() {
+    function Tarai() {}
+
+    Tarai.prototype.init = function(x, y, z) {
+      this.list = [];
+      this.index = 0;
+      return this.tarai(x, y, z);
     };
-    Tarai = (function() {
-      function Tarai() {}
 
-      Tarai.prototype.init = function(x, y, z) {
-        this.list = [];
-        this.index = 0;
-        return this.tarai(x, y, z);
-      };
-
-      Tarai.prototype.tarai = function(x, y, z) {
-        this.list.push([x, y, z]);
-        if (x <= y) {
-          return y;
-        } else {
-          return this.tarai(this.tarai(x - 1, y, z), this.tarai(y - 1, z, x), this.tarai(z - 1, x, y));
-        }
-      };
-
-      Tarai.prototype.fetch = function() {
-        return this.list[this.index++];
-      };
-
-      Tarai.prototype.reset = function() {
-        return this.index = 0;
-      };
-
-      return Tarai;
-
-    })();
-    tarai = new Tarai;
-    tarai.init(10, 5, 0);
-    scale = new sc.Scale([0, 2, 3, 7, 9], 12, 'Kumoi');
-    baseRoot = 62;
-    pattern = [0, 0, 1, 1, 2, 2, 1, 1, 0, 0, 1, 1, 2, 2, 1, 1];
-    car = [];
-    synth = T('PluckGen');
-    master = T('chorus', {
-      delay: 4,
-      rate: 1,
-      depth: 40
-    }, synth);
-    master = T('delay', {
-      time: 'bpm120 l8.',
-      fb: 0.4,
-      mix: 0.3
-    }, master);
-    timer = T('interval', {
-      interval: 'bpm120 l16'
-    }, function(count) {
-      var i, noteNum, _ref;
-      count &= 15;
-      if (count === 0) {
-        car = (_ref = tarai.fetch()) != null ? _ref.sorted() : void 0;
-      }
-      if (car) {
-        i = car[pattern[count]];
-        noteNum = Math.round(scale.performDegreeToKey(i)) + baseRoot;
-        noteNum += 12 * (count % 2);
-        return synth.noteOn(noteNum, 100);
-      }
-    });
-    isPlaying = false;
-    return $('#play').on('click', function() {
-      isPlaying = !isPlaying;
-      if (isPlaying) {
-        tarai.reset();
-        master.play();
-        timer.start();
-        return $(this).addClass('btn-active');
+    Tarai.prototype.tarai = function(x, y, z) {
+      this.list.push([x, y, z]);
+      if (x <= y) {
+        return y;
       } else {
-        master.pause();
-        timer.stop();
-        return $(this).removeClass('btn-active');
+        return this.tarai(this.tarai(x - 1, y, z), this.tarai(y - 1, z, x), this.tarai(z - 1, x, y));
       }
-    });
+    };
+
+    Tarai.prototype.fetch = function() {
+      return this.list[this.index++];
+    };
+
+    Tarai.prototype.reset = function() {
+      return this.index = 0;
+    };
+
+    return Tarai;
+
+  })();
+
+  tarai = new Tarai;
+
+  scale = new sc.Scale([0, 2, 3, 7, 9], 12, 'Kumoi');
+
+  baseRoot = 62;
+
+  pattern = [0, 0, 1, 1, 2, 2, 1, 1, 0, 0, 1, 1, 2, 2, 1, 1];
+
+  car = [];
+
+  synth = T('PluckGen');
+
+  master = T('chorus', {
+    delay: 4,
+    rate: 1,
+    depth: 40
+  }, synth);
+
+  master = T('delay', {
+    time: 'bpm120 l8.',
+    fb: 0.4,
+    mix: 0.3
+  }, master);
+
+  timer = T('interval', {
+    interval: 'bpm120 l16'
+  }, function(count) {
+    var i, noteNum, p, _ref;
+    i = count & 15;
+    if (i === 0) {
+      car = (_ref = tarai.fetch()) != null ? _ref.sort() : void 0;
+    }
+    if (car) {
+      p = pattern[i];
+      i = car[p];
+      switch (p) {
+        case 0:
+          vue.x = clip(i, 0, 10);
+          break;
+        case 1:
+          vue.y = clip(i, 0, 10);
+          break;
+        case 2:
+          vue.z = clip(i, 0, 10);
+      }
+      noteNum = Math.round(scale.performDegreeToKey(i)) + baseRoot;
+      noteNum += 12 * (count % 2);
+      return synth.noteOn(noteNum, 100);
+    }
   });
+
+  app = new ((function() {
+    function _Class() {
+      this.isPlaying = false;
+    }
+
+    _Class.prototype.init = function(x, y, z) {
+      return tarai.init(x, y, z);
+    };
+
+    _Class.prototype.play = function() {
+      this.isPlaying = true;
+      tarai.reset();
+      master.play();
+      return timer.start();
+    };
+
+    _Class.prototype.stop = function() {
+      this.isPlaying = false;
+      master.pause();
+      return timer.stop();
+    };
+
+    return _Class;
+
+  })());
+
+  vue = new Vue({
+    el: '#app',
+    data: {
+      x: 10,
+      y: 5,
+      z: 0,
+      xs: _.range(11).map(function(x) {
+        return {
+          value: x
+        };
+      }),
+      ys: _.range(11).map(function(y) {
+        return {
+          value: y
+        };
+      }),
+      zs: _.range(11).map(function(z) {
+        return {
+          value: z
+        };
+      }),
+      isPlaying: app.isPlaying
+    },
+    methods: {
+      play: function() {
+        if (app.isPlaying) {
+          app.stop();
+        } else {
+          window.location.replace("#" + this.x + "," + this.y + "," + this.z);
+          app.init(this.x, this.y, this.z);
+          app.play();
+        }
+        return this.isPlaying = app.isPlaying;
+      },
+      stop: function() {
+        if (app.isPlaying) {
+          app.stop();
+        }
+        return this.isPlaying = app.isPlaying;
+      },
+      tweet: function() {
+        var text, url, x, y, z;
+        x = clip(this.x, 0, 10);
+        y = clip(this.y, 0, 10);
+        z = clip(this.z, 0, 10);
+        text = 'tarai-music';
+        url = "http://" + location.host + "/tarai/#" + x + "," + y + "," + z;
+        return utils.tweet({
+          text: text,
+          url: url
+        });
+      }
+    }
+  });
+
+  window.onhashchange = function() {
+    var items;
+    items = decodeURI(location.hash.substr(1).trim()).split(',');
+    vue.x = clip(items[0] | 0, 0, 10);
+    vue.y = clip(items[1] | 0, 0, 10);
+    return vue.z = clip(items[2] | 0, 0, 10);
+  };
+
+  if (location.hash) {
+    window.onhashchange();
+  }
 
 }).call(this);
